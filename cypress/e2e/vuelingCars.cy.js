02ad6c79d6503ad533eefa6a899f983ee05a4bec
martinNahuelGomez <rekrutmartingomez@gmail.com>
2024-11-21 15:17:44 +0100

    Revert "Refactor fixture"
    This reverts commit e9cef605b68f3904a61289da05bdd47ff5054486.

--- cypress/e2e/vuelingCars.cy.js
+++ cypress/e2e/vuelingCars.cy.js
@@ -2,44 +2,43 @@ describe('Vueling cars rental page tests', () => {
   beforeEach(() => {
     cy.viewport(430, 932)
     cy.visit('https://cars.vueling.com/')
-    cy.fixture('data.json').then((data) => {
-      cy.fixture('locators.json').then((locators) => {
-        cy.get(locators.submitButton).should('be.visible')
-        cy.get(locators.pickupLocationInput).type(data.pickupLocation)
-        cy.get(locators.pickupLocationAirportDropdown).contains('li', data.pickupLocationDropdownMenuOption).click()
-        cy.setDateFromToday(locators.pickupDate, 3)
-        cy.setDateFromToday(locators.returnDate, 5)
-        cy.selectDriverAge(data.driverAge)
-        // TODO: Clicking the search button is not working and therefore i added the cy visit to the a resulting url of the search (dates are hardcoded in the url for now)
-        // cy.get(locators.submitButton).invoke('removeAttr', 'target').click()
-        cy.visit(data.skipingSearchButtonURL)
-        cy.wait(7000)
-        cy.scrollTo('bottom')
-        cy.wait(1000)
-        cy.scrollTo('bottom')
-        cy.get(`${locators.carList} ${locators.carListItem}`).contains('SUV').parents(locators.carListItem).find(locators.selectCarButton).click({force: true})
-        cy.get(locators.selectedCar).should('contain', 'SUV')
-      })
+    cy.fixture('testData.json').then((testData) => {
+      const locators = testData.locators
+      const data = testData.data
+      cy.get(locators.submitButton).should('be.visible')
+      cy.get(locators.pickupLocationInput).type(data.pickupLocation)
+      cy.get(locators.pickupLocationAirportDropdown).contains('li', data.pickupLocationDropdownMenuOption).click()
+      cy.setDateFromToday(locators.pickupDate, 3)
+      cy.setDateFromToday(locators.returnDate, 5)
+      cy.selectDriverAge(data.driverAge)
+      // TODO: Clicking the search button is not working and therefore i added the cy visit to the a resulting url of the search (dates are hardcoded in the url for now)
+      // cy.get(locators.submitButton).invoke('removeAttr', 'target').click()
+      cy.visit(data.skipingSearchButtonURL)
+      cy.wait(7000)
+      cy.scrollTo('bottom')
+      cy.wait(1000)
+      cy.scrollTo('bottom')
+      cy.get(`${locators.carList} ${locators.carListItem}`).contains('SUV').parents(locators.carListItem).find(locators.selectCarButton).click({force: true})
+      cy.get(locators.selectedCar).should('contain', 'SUV')
     })
     cy.clearCookies()
   })
 
   it('Attempt to rent with basic rate and assert insurance disclaimer', () => {
-    cy.fixture('data.json').then((data) => {
-      const insuranceType = data.insurance.basic
+    cy.fixture('testData.json').then((testData) => {
+      const insuranceType = testData.data.insurance.basic
       cy.selectInsuranceTypeAndAssertCostDifference(insuranceType)
     })
   })
   
   it('Attempt to rent with premium rate and assert premium coverage at final price list of items', () => {
-    cy.fixture('data.json').then((data) => {
-      cy.fixture('locators.json').then((locators) => {
-        const insuranceType = data.insurance.premium
+    cy.fixture('testData.json').then((testData) => {
+      const locators = testData.locators
+      const insuranceType = testData.data.insurance.premium
   
-        cy.selectInsuranceTypeAndAssertCostDifference(insuranceType)
-        cy.get(locators.finalPriceDisplayButton).click()
-        cy.get(locators.finalPriceRow).contains('Cobertura Premium').should('be.visible')
-      })
+      cy.selectInsuranceTypeAndAssertCostDifference(insuranceType)
+      cy.get(locators.finalPriceDisplayButton).click()
+      cy.get(locators.finalPriceRow).contains('Cobertura Premium').should('be.visible')
     })
   })
 })
